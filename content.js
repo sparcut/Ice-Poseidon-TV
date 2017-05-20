f4759adfa72333127aa13453b5b442dffe4f91de
Jertzukka <Jertzukka@gmail.com>
2017-05-20 10:29:52 +0300

    Mention fixes

--- content.js
+++ content.js
@@ -275,7 +275,7 @@ var checkIfOnStreamPage = function() {
 var addObserverIfDesiredNodeAvailable = function () {
 
     var target = document.querySelector('.style-scope .yt-live-chat-item-list-renderer');
-    var authorname = $('#author #author-name').text();
+    var authorname = $('#author #author-name').text().toLowerCase();
 
     if (!target) {
         setTimeout(addObserverIfDesiredNodeAvailable, 250);
@@ -300,9 +300,9 @@ var addObserverIfDesiredNodeAvailable = function () {
 
                     if(mentionHighlight && authorname.length > 2 && !$node.hasClass('yt-live-chat-legacy-paid-message-renderer-0')) { // Check it's not sponsor / superchat, also mentionHighlight enabled
                         var uniqueid = $node.get(0).getAttribute('id') // Copy unique message id
-                        var message = $node.find('#message').text();
+                        var message = (" " + $node.find('#message').text().toLowerCase() + " ").replace(/[\u200B-\u200D\uFEFF]/g, '');
 
-                        if(message.toLowerCase().indexOf(authorname.toLowerCase()) !== -1 & uniqueid.length > 30) { // If your name is in the message, and it's not your message
+                        if(uniqueid.length > 30 && (message.indexOf(' '+authorname+' ') !== -1 || message.indexOf(' @'+authorname+' ') !== -1)) { // If your name is in the message, and it's not your message
                             $node.get(0).style.backgroundColor = "rgba(255,0,0,0.40)";
                             $node.find('#author-name').get(0).style.color = "#ffffff";
                         }
