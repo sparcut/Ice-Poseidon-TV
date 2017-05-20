a0296aa1bf4ee908584bc84fcf13d113f190bd7f
Jertzukka <Jertzukka@gmail.com>
2017-05-20 10:51:22 +0300

    Fixes for mention highlight

--- content.js
+++ content.js
@@ -302,7 +302,7 @@ var addObserverIfDesiredNodeAvailable = function () {
                         var uniqueid = $node.get(0).getAttribute('id') // Copy unique message id
                         var message = (" " + $node.find('#message').text().toLowerCase() + " ").replace(/[\u200B-\u200D\uFEFF]/g, '');
 
-                        if(uniqueid.length > 30 && (message.indexOf(' '+authorname+' ') !== -1 || message.indexOf(' @'+authorname+' ') !== -1)) { // If your name is in the message, and it's not your message
+                        if(uniqueid.length > 30 && (message.indexOf(' '+authorname+' ') !== -1 || message.indexOf('@'+authorname+' ') !== -1)) { // If your name is in the message, and it's not your message
                             $node.get(0).style.backgroundColor = "rgba(255,0,0,0.40)";
                             $node.find('#author-name').get(0).style.color = "#ffffff";
                         }
