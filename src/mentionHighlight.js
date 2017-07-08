e6a75c7249b3990447d1a3ec7a0d681e1c4b7de1
xBytez <git@xbytez.io>
2017-07-09 00:20:17 +0200

    Fix bug created in #96. No longer makes every message red if you are named "ice poseidon". Doesn't allow fake "ice poseidon" usernames to be highlighted, only the streamer.

--- src/mentionHighlight.js
+++ src/mentionHighlight.js
@@ -6,13 +6,14 @@ import { getOptions } from './main';
  */
 export default function MentionHighlight(node)
 {
-    const authorname = $('#author #author-name').text().toLowerCase();
+    const currentUser = $('#author #author-name').text().toLowerCase();
+    const authorType = node.get(0).getAttribute('author-type');
 
-    if (typeof authorname === 'undefined') {
+    if (typeof currentUser === 'undefined') {
         return;
     }
 
-    if (getOptions()['mentionHighlight'] && authorname.length > 2 && !node.hasClass('yt-live-chat-legacy-paid-message-renderer-0')) { // Check it's not sponsor / superchat, also mentionHighlight enabled
+    if (getOptions()['mentionHighlight'] && currentUser.length > 2 && !node.hasClass('yt-live-chat-legacy-paid-message-renderer-0')) { // Check it's not sponsor / superchat, also mentionHighlight enabled
 
         const uniqueid = node.get(0).getAttribute('id'); // Copy unique message id
         const message = (" " + node.find('#message').text().toLowerCase() + " ").replace(/[\u200B-\u200D\uFEFF]/g, '');
@@ -21,7 +22,7 @@ export default function MentionHighlight(node)
             return;
         }
 
-        if (uniqueid.length > 30 && (authorname == "ice poseidon" || message.indexOf(' '+authorname+' ') !== -1 || message.indexOf('@'+authorname+' ') !== -1)) { // If your name is in the message, and it's not your message
+        if (uniqueid.length > 30 && (authorType === "owner" || message.indexOf(' '+currentUser+' ') !== -1 || message.indexOf('@'+currentUser+' ') !== -1)) { // If your name is in the message, and it's not your message or the message is from the streamer
             node.get(0).style.backgroundColor = "rgba(255,0,0,0.40)";
             node.find('#author-name').get(0).style.color = "#ffffff";
         }
